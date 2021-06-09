45b748142eb868024d535445ef5ee23cfef30d9d
Spencer Carli <spencercarli@gmail.com>
2021-06-09 12:55:20 -0500

    Setup prisma

---
+++ server/prisma/migrations/20210609203923_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "soldCount" INTEGER NOT NULL DEFAULT 0
+);

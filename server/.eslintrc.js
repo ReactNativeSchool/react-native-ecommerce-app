2afa6a5b3b65d7a00bb1cc94e2a96d83a4cb591f
Spencer Carli <spencercarli@gmail.com>
2021-06-09 10:06:12 -0500

    Prettier and linting

---
+++ server/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['handlebarlabs', 'plugin:prettier/recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+  globals: {},
+  plugins: [],
+};

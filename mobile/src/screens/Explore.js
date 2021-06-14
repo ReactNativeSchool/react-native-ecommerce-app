40ea6df9db993314cb75fb3e2f70ffd36e8a8a2f
Spencer Carli <spencercarli@gmail.com>
2021-06-14 10:55:24 -0500

    Render data on explore page

--- mobile/src/screens/Explore.js
+++ mobile/src/screens/Explore.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Loading } from '../components/Loading';
 import { useExploreData } from '../util/api';
+import { ProductList } from '../components/List';
 
 export const Explore = () => {
   const res = useExploreData();
@@ -11,6 +12,11 @@ export const Explore = () => {
     return <Loading />;
   }
 
-  console.log(data);
-  return null;
+  const sections = data.categories.map(category => ({
+    title: category.name,
+    data: category.products,
+    items: undefined,
+  }));
+
+  return <ProductList sections={sections} />;
 };

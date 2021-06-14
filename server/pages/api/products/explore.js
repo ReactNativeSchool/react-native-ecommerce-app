73e5f0c78f7e7df2f618a23b22fe88b31268dc93
Spencer Carli <spencercarli@gmail.com>
2021-06-14 10:39:53 -0500

    Explore endpoint

---
+++ server/pages/api/products/explore.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const categories = await prisma.category.findMany({
+    // Only get categories that have at least one product in them
+    where: {
+      products: { some: {} },
+    },
+    orderBy: {
+      name: 'asc',
+    },
+    select: {
+      id: true,
+      name: true,
+      // we also want to grab the child products
+      products: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+          price: true,
+        },
+      },
+    },
+  });
+
+  return res.status(200).json({ categories });
+};

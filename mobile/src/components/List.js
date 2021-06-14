5ebc17fef4bb211c968878b0c72a5c54cecc0fd6
Spencer Carli <spencercarli@gmail.com>
2021-06-14 10:49:12 -0500

    Create reusable ProductList component

--- mobile/src/components/List.js
+++ mobile/src/components/List.js
@@ -5,7 +5,9 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
+  SectionList,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Text } from './Text';
 import colors from '../constants/colors';
@@ -40,6 +42,12 @@ const styles = StyleSheet.create({
   },
   itemCard: { flex: 1, padding: 10 },
   cardTitle: { fontWeight: 'bold', marginVertical: 5 },
+  sectionList: {
+    backgroundColor: colors.background,
+  },
+  content: {
+    paddingBottom: 100,
+  },
 });
 
 export const ListItem = ({ title, subtitle, onPress = () => null }) => {
@@ -78,3 +86,50 @@ export const ItemCard = ({ name, price, image, onPress }) => (
     <Text>{money(price)}</Text>
   </TouchableOpacity>
 );
+
+export const ProductList = ({ sections = [] }) => {
+  const navigation = useNavigation();
+
+  return (
+    <SectionList
+      style={styles.sectionList}
+      contentContainerStyle={styles.content}
+      sections={sections}
+      renderItem={({ index, section }) => {
+        // We do this to simulate numColumns in FlatList. This will only work for 2 column layout.
+
+        // We only want to do this on odd columns so we don't duplicate items.
+        if (index % 2 !== 0) return null;
+
+        const item = section.data[index];
+        const nextItem = section.data[index + 1];
+
+        const onPress = () => {
+          navigation.push('Details');
+        };
+
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              backgroundColor: '#fff',
+            }}
+          >
+            <ItemCard {...item} onPress={onPress} />
+            {nextItem ? (
+              <ItemCard {...nextItem} onPress={onPress} />
+            ) : (
+              <View style={{ flex: 1 }} />
+            )}
+          </View>
+        );
+      }}
+      renderSectionHeader={({ section }) => (
+        <SectionHeader>{section.title}</SectionHeader>
+      )}
+      renderSectionFooter={() => <SectionFooter />}
+      stickySectionHeadersEnabled={false}
+    />
+  );
+};
--- mobile/src/screens/Home.js
+++ mobile/src/screens/Home.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, SectionList, View } from 'react-native';
 
-import { SectionHeader, SectionFooter, ItemCard } from '../components/List';
+import { ProductList } from '../components/List';
 import { Loading } from '../components/Loading';
 import { useHomeData } from '../util/api';
-import colors from '../constants/colors';
 
-const styles = StyleSheet.create({
-  sectionList: {
-    backgroundColor: colors.background,
-  },
-  content: {
-    paddingBottom: 100,
-  },
-});
-
-export const Home = ({ navigation }) => {
+export const Home = () => {
   const res = useHomeData();
   const { isLoading, data } = res;
 
@@ -29,46 +18,5 @@ export const Home = ({ navigation }) => {
     items: undefined,
   }));
 
-  return (
-    <SectionList
-      style={styles.sectionList}
-      contentContainerStyle={styles.content}
-      sections={sections}
-      renderItem={({ index, section }) => {
-        // We do this to simulate numColumns in FlatList. This will only work for 2 column layout.
-
-        // We only want to do this on odd columns so we don't duplicate items.
-        if (index % 2 !== 0) return null;
-
-        const item = section.data[index];
-        const nextItem = section.data[index + 1];
-
-        const onPress = () => {
-          navigation.push('Details');
-        };
-
-        return (
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              backgroundColor: '#fff',
-            }}
-          >
-            <ItemCard {...item} onPress={onPress} />
-            {nextItem ? (
-              <ItemCard {...nextItem} onPress={onPress} />
-            ) : (
-              <View style={{ flex: 1 }} />
-            )}
-          </View>
-        );
-      }}
-      renderSectionHeader={({ section }) => (
-        <SectionHeader>{section.title}</SectionHeader>
-      )}
-      renderSectionFooter={() => <SectionFooter />}
-      stickySectionHeadersEnabled={false}
-    />
-  );
+  return <ProductList sections={sections} />;
 };

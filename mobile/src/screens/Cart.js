e4af665e73b1614bea7de4eb942a8017236a5a7f
Spencer Carli <spencercarli@gmail.com>
2021-06-15 11:59:36 -0500

    Empty cart state

--- mobile/src/screens/Cart.js
+++ mobile/src/screens/Cart.js
@@ -1,1 +1,24 @@
-export const Cart = () => null;
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { Text } from '../components/Text';
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingTop: 60,
+    alignItems: 'center',
+  },
+});
+
+export const Cart = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Text type="header" style={{ fontSize: 25 }}>
+        YOUR CART IS EMPTY
+      </Text>
+    </View>
+  );
+};

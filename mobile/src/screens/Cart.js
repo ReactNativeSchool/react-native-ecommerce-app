4b4031c25938f049817e4796730378ba75b67a47
Spencer Carli <spencercarli@gmail.com>
2021-06-16 10:06:10 -0500

    Calculate Cart Total

--- mobile/src/screens/Cart.js
+++ mobile/src/screens/Cart.js
@@ -3,8 +3,9 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 
 import { Text } from '../components/Text';
 import colors from '../constants/colors';
-import { useCart } from '../util/cart';
+import { useCart, cartTotal } from '../util/cart';
 import { CartRow } from '../components/CartRow';
+import { money } from '../util/format';
 
 const styles = StyleSheet.create({
   emptyContainer: {
@@ -15,6 +16,12 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: colors.white,
+  },
+  summaryContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderTopColor: colors.border,
+    borderTopWidth: StyleSheet.hairlineWidth,
   },
 });
 
@@ -37,6 +44,12 @@ export const Cart = () => {
       {Object.keys(cart).map(id => {
         return <CartRow key={id} id={id} />;
       })}
+      <View style={styles.summaryContainer}>
+        <Text>
+          <Text style={{ fontWeight: 'bold' }}>Total: </Text>
+          {money(cartTotal(cart))}
+        </Text>
+      </View>
     </ScrollView>
   );
 };
--- mobile/src/util/cart.js
+++ mobile/src/util/cart.js
@@ -2,6 +2,17 @@ import create from 'zustand';
 import { persist } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const cartTotal = cart => {
+  let total = 0;
+
+  Object.keys(cart).forEach(id => {
+    const item = cart[id];
+    total += item.price * item.quantity;
+  });
+
+  return total;
+};
+
 export const useCart = create(
   persist(
     set => ({

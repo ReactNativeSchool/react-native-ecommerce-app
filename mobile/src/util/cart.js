706c39daf5ef7442684ba5ed7b122201787daa0f
Spencer Carli <spencercarli@gmail.com>
2021-06-16 09:52:12 -0500

    Persist Cart between Sessions

--- mobile/src/util/cart.js
+++ mobile/src/util/cart.js
@@ -1,39 +1,49 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const useCart = create(set => ({
-  cart: {},
-  addItem: ({ id, name, price }) =>
-    set(state => {
-      const cart = { ...state.cart };
-      if (!cart[id]) {
-        cart[id] = {
-          id,
-          name,
-          price,
-          quantity: 0,
-        };
-      }
+export const useCart = create(
+  persist(
+    set => ({
+      cart: {},
+      addItem: ({ id, name, price }) =>
+        set(state => {
+          const cart = { ...state.cart };
+          if (!cart[id]) {
+            cart[id] = {
+              id,
+              name,
+              price,
+              quantity: 0,
+            };
+          }
 
-      cart[id].quantity += 1;
+          cart[id].quantity += 1;
 
-      return { cart };
+          return { cart };
+        }),
+      removeItem: ({ id }) =>
+        set(state => {
+          const cart = { ...state.cart };
+
+          if (!cart[id]) {
+            return { cart };
+          }
+
+          const quantity = cart[id].quantity - 1;
+
+          if (quantity <= 0) {
+            delete cart[id];
+          } else {
+            cart[id].quantity = quantity;
+          }
+
+          return { cart };
+        }),
     }),
-  removeItem: ({ id }) =>
-    set(state => {
-      const cart = { ...state.cart };
-
-      if (!cart[id]) {
-        return { cart };
-      }
-
-      const quantity = cart[id].quantity - 1;
-
-      if (quantity <= 0) {
-        delete cart[id];
-      } else {
-        cart[id].quantity = quantity;
-      }
-
-      return { cart };
-    }),
-}));
+    {
+      name: 'cart',
+      getStorage: () => AsyncStorage,
+    },
+  ),
+);

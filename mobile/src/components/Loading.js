a4ab5be768f2e241b583aab3c4e406e2810f4f36
Spencer Carli <spencercarli@gmail.com>
2021-06-24 11:34:58 -0500

    Set color of loading indicator. Resolves #1.

--- mobile/src/components/Loading.js
+++ mobile/src/components/Loading.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
+import colors from '../constants/colors';
 
 export const Loading = () => (
   <View style={{ paddingVertical: 20 }}>
-    <ActivityIndicator size="large" />
+    <ActivityIndicator size="large" color={colors.brand} />
   </View>
 );
